dfe4f8c58fc1ab70f3e235bff3c0a4d7cccfb056
Yura Ivanov <vanov_yura@mail.rugit config --global user.name Yura>
2022-09-27 16:32:45 +0000

    Initial commit

---
+++ freelance_pyhton/freelance_pyhton/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from freelance_pyhton.users.models import User
+from freelance_pyhton.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
